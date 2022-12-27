dac957aa7f2fdb27637eeb38603fb005d8956187
ilovetheseapps <ilovetheseapps@gmail.com>
2022-12-27 12:29:20 +0545

    initial commit

---
+++ lib/dutch_text_delegate.dart
@@ -0,0 +1,45 @@
+import 'package:image_painter/image_painter.dart';
+
+class DutchTextDelegate implements TextDelegate {
+  @override
+  String get arrow => "ARROW";
+
+  @override
+  String get changeBrushSize => "Penseelgrootte Wijzigen";
+
+  @override
+  String get changeColor => "Verander Kleur";
+
+  @override
+  String get changeMode => "CHANGE MODE";
+
+  @override
+  String get circle => "CIRCLE";
+
+  @override
+  String get clearAllProgress => "Alle Voortgang Wissen";
+
+  @override
+  String get dashLine => "Dashlane";
+
+  @override
+  String get done => "DONE";
+
+  @override
+  String get drawing => "Tekening";
+
+  @override
+  String get line => "LINE";
+
+  @override
+  String get noneZoom => "Geen / Zoom";
+
+  @override
+  String get rectangle => "Rectangle";
+
+  @override
+  String get text => "TEXT";
+
+  @override
+  String get undo => "ongedaan maken";
+}
